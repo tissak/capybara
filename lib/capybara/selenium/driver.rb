5eed4e74462169b9734e8de7cc888264fdb8192f
Tony Issakov <tony@thefrontiergroup.com.au>
2012-07-31 12:05:42 +0800

    temporary commit to remove hanging with chromedriver

--- lib/capybara/selenium/driver.rb
+++ lib/capybara/selenium/driver.rb
@@ -68,13 +68,14 @@ class Capybara::Selenium::Driver < Capybara::Driver::Base
   def reset!
     # Use instance variable directly so we avoid starting the browser just to reset the session
     if @browser
-      begin @browser.manage.delete_all_cookies
+      begin
+        # @browser.manage.delete_all_cookies
       rescue Selenium::WebDriver::Error::UnhandledError
         # delete_all_cookies fails when we've previously gone
         # to about:blank, so we rescue this error and do nothing
         # instead.
       end
-      @browser.navigate.to('about:blank')
+      # @browser.navigate.to('about:blank')
     end
   end
 
